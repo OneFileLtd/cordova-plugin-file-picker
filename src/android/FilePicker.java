b4e2b68e5dd8455989e5b6b0f0a2c0bcd8d28fd7
Martin Howarth <mhowarth@onefile.co.uk>
2017-04-05 16:04:17 +0100

    Allow importing of all files, error reporting where required. Still an issue, where a file can only be imported once.

--- src/android/FilePicker.java
+++ src/android/FilePicker.java
@@ -1,5 +1,6 @@
 package uk.co.onefile.nomadionic.filepicker;
 
+import android.app.Activity;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -35,6 +36,7 @@ import android.content.ContextWrapper;
 public class FilePicker extends CordovaPlugin {
 	public final static int PICK_FROM_GALLERY_CODE = 1046;
 	private static final int REQUEST_WRITE_PERMISSION = 786;
+	private static final int REQUEST_READ_PERMISSION = 787;
 	private static final int STATUS_ERROR = 0;
 	private static final int STATUS_SUCCESSFUL = 1;
 	private static final String VIDEO_3GPP = "video/3gpp";
@@ -51,7 +53,6 @@ public class FilePicker extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		Log.i("FilePicker", action);
 		if (action.equals("pickFile")) {
 			final JSONObject config = args.getJSONObject(0);
 			cordova.getThreadPool().execute(new Runnable() {
@@ -76,28 +77,27 @@ public class FilePicker extends CordovaPlugin {
 	}
 
 	private void requestPermission() {
-		boolean needExternalStoragePermission = !PermissionHelper.hasPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-
+		boolean needReadExternalStoragePermission = !PermissionHelper.hasPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+		boolean needWriteExternalStoragePermission = !PermissionHelper.hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			if (needExternalStoragePermission) {
-				Log.i("FilePicker", "requestPermission - onPickPhoto - permission required");
-				PermissionHelper.requestPermission(this, REQUEST_WRITE_PERMISSION, Manifest.permission.READ_EXTERNAL_STORAGE);
+			if (needReadExternalStoragePermission) {
+				PermissionHelper.requestPermission(this, REQUEST_READ_PERMISSION, Manifest.permission.READ_EXTERNAL_STORAGE);
+			} else if (needWriteExternalStoragePermission) {
+				PermissionHelper.requestPermission(this, REQUEST_WRITE_PERMISSION, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 			} else {
-				Log.i("FilePicker", "requestPermission - onPickPhoto - permission required, and granted");
 				onPickPhoto(currentWebView.getView());
 			}
 		} else {
-			Log.i("FilePicker", "requestPermission - onPickPhoto - permission not required");
 			onPickPhoto(currentWebView.getView());
 		}
 	}
 
 	public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
-		Log.i("onRequestPermissionsRes", "requestCode:" + requestCode);
-		Log.i("onRequestPermissionsRes", permissions.toString());
-		Log.i("onRequestPermissionsRes", grantResults.toString());
-		if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-			Log.i("FilePicker", "onRequestPermissionsResult - onPickPhoto");
+		Boolean permission = false;
+		for (int i = 0; i < grantResults.length; i++) {
+			permission &= ((grantResults[i] == PackageManager.PERMISSION_GRANTED));
+		}
+		if (permission) {
 			onPickPhoto(currentWebView.getView());
 		} else
 		{
@@ -107,126 +107,141 @@ public class FilePicker extends CordovaPlugin {
 
 	public void onPickPhoto(View view) {
 		PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-		// Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-		// intent.setType("image/* video/*");
+		Intent intent;
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-			Log.i("onPickPhoto", "<19");
-			Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-			intent.setType("image/* video/*");
-			if (intent.resolveActivity(packageManager) != null) {
-				Log.i("FilePicker", "onPickPhoto");
-				this.cordova.startActivityForResult((CordovaPlugin) this, intent, PICK_FROM_GALLERY_CODE);
-			}
+			intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			intent.setType("image/* video/* audio/*");
 		} else {
-			Log.i("onPickPhoto", ">=19");
-			Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+			intent = new Intent(Intent.ACTION_GET_CONTENT);
 			intent.setType("*/*");
-			intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/*", "video/*"});
-			if (intent.resolveActivity(packageManager) != null) {
-				Log.i("FilePicker", "onPickPhoto");
-				this.cordova.startActivityForResult((CordovaPlugin) this, intent, PICK_FROM_GALLERY_CODE);
-			}
+			intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/*", "video/*", "audio/*", "text/*", "application/*"});
 		}
-
+		if (intent.resolveActivity(packageManager) != null) {
+			this.cordova.startActivityForResult((CordovaPlugin) this, intent, PICK_FROM_GALLERY_CODE);
+		}
 	}
 
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		final Intent dataInner = data;
-		Log.i("onActivityResult", "build-version:" + Build.VERSION.SDK_INT);
-		Log.i("onActivityResult", data.getDataString());
-		cordova.getThreadPool().execute(new Runnable() {
-			public void run() {
-				if (dataInner != null) {
-					Log.i("onActivityResult", dataInner.getDataString());
-					Uri photoUri = dataInner.getData();
-					JSONArray result = new JSONArray();
-					JSONObject file;
-					if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-						Log.i("onActivityResult", "<19");
+	public void onActivityResult(int requestCode, int resultCode, final Intent data) {
+		if(requestCode == PICK_FROM_GALLERY_CODE) {
+			if (resultCode == cordova.getActivity().RESULT_OK) {
+				if (data == null) {
+					currentCallbackContext.error("no data");
+					return;
+				}
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						Uri photoUri = data.getData();
+						JSONArray result = new JSONArray();
+						JSONObject file;
 						file = createMediaFile(photoUri);
-					} else {
-						Log.i("onActivityResult", ">=19");
-						file = createMediaFile(photoUri);
-					}
-					if(file == null) {
-						Log.i("onActivityResult", "no file data");
-						currentCallbackContext.error("no file data available, please check permissions");
+						if (file == null) {
+							currentCallbackContext.error("no file data available, please check permissions");
+							return;
+						}
+						result.put(file);
+						currentCallbackContext.success(result);
 						return;
 					}
-					result.put(file);
-					currentCallbackContext.success(result);
-				}
+				});
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				currentCallbackContext.error("user cancelled");
+			} else if (resultCode == Activity.RESULT_FIRST_USER) {
 			}
-		});
+		}
+	}
+
+	@Override
+	public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext)
+	{
 	}
 
 	private JSONObject createMediaFile(Uri data) {
-		File fp = new File(getPath(cordova.getActivity().getApplicationContext(), data)); //currentWebView.getResourceApi().mapUriToFile(data);
-		JSONObject obj = new JSONObject();
-
-		Class webViewClass = currentWebView.getClass();
-		PluginManager pm = null;
 		try {
-			Method gpm = webViewClass.getMethod("getPluginManager");
-			pm = (PluginManager) gpm.invoke(currentWebView);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-			currentCallbackContext.error(e.getMessage());
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-			currentCallbackContext.error(e.getMessage());
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-			currentCallbackContext.error(e.getMessage());
-		}
-		if (pm == null) {
+			if(data == null) {
+				return null;
+			}
+
+			File fp = new File(getPath(cordova.getActivity().getApplicationContext(), data));
+			if (fp.exists()) {
+			} else {
+				return null;
+			}
+			JSONObject obj = new JSONObject();
+
+			Class webViewClass = currentWebView.getClass();
+			PluginManager pm = null;
 			try {
-				Field pmf = webViewClass.getField("pluginManager");
-				pm = (PluginManager)pmf.get(currentWebView);
-			} catch (NoSuchFieldException e) {
+				Method gpm = webViewClass.getMethod("getPluginManager");
+				pm = (PluginManager) gpm.invoke(currentWebView);
+			} catch (NoSuchMethodException e) {
 				e.printStackTrace();
-				currentCallbackContext.error(e.getMessage());
+				return null;
 			} catch (IllegalAccessException e) {
 				e.printStackTrace();
-				currentCallbackContext.error(e.getMessage());
+				return null;
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+				return null;
 			}
-		}
-		FileUtils filePlugin = (FileUtils) pm.getPlugin("File");
-		LocalFilesystemURL url = filePlugin.filesystemURLforLocalPath(fp.getAbsolutePath());
+			if (pm == null) {
+				try {
+					Field pmf = webViewClass.getField("pluginManager");
+					pm = (PluginManager) pmf.get(currentWebView);
+				} catch (NoSuchFieldException e) {
+					e.printStackTrace();
+					return null;
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+			FileUtils filePlugin = (FileUtils) pm.getPlugin("File");
+			LocalFilesystemURL url = filePlugin.filesystemURLforLocalPath(fp.getAbsolutePath());
 
-		try {
-			obj.put("name", fp.getName());
-			obj.put("fullPath", Uri.fromFile(fp));
-			if (url != null) {
-				obj.put("localURL", url.toString());
+			try {
+				obj.put("name", fp.getName());
+				obj.put("fullPath", Uri.fromFile(fp));
+				if (url != null) {
+					obj.put("localURL", url.toString());
+				}
+				if (fp.getAbsoluteFile().toString().endsWith(".3gp") || fp.getAbsoluteFile().toString().endsWith(".3gpp")) {
+					obj.put("type", VIDEO_3GPP);
+				} else {
+					obj.put("type", FileHelper.getMimeType(Uri.fromFile(fp), cordova));
+				}
+
+				obj.put("lastModifiedDate", fp.lastModified());
+				obj.put("size", fp.length());
+			} catch (JSONException e) {
+				e.printStackTrace();
+				return null;
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+				return null;
 			}
-			if (fp.getAbsoluteFile().toString().endsWith(".3gp") || fp.getAbsoluteFile().toString().endsWith(".3gpp")) {
-				obj.put("type", VIDEO_3GPP);
+			if (obj.isNull("name")) {
 			} else {
-				obj.put("type", FileHelper.getMimeType(Uri.fromFile(fp), cordova));
+				try {
+					Log.i("createMediaFile", obj.toString(2));
+				} catch (JSONException e) {
+					e.printStackTrace();
+					return null;
+				}
 			}
-
-			obj.put("lastModifiedDate", fp.lastModified());
-			obj.put("size", fp.length());
-		} catch (JSONException e) {
-			e.printStackTrace();
-			currentCallbackContext.error(e.getMessage());
+			return obj;
 		} catch (NullPointerException e) {
 			e.printStackTrace();
-			currentCallbackContext.error(e.getMessage());
 		}
-		return obj;
+		return null;
 	}
 
-	public static String getPath(Context context, Uri uri) {
+	public static String getPath(final Context context, final Uri uri) {
+
 		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
 
-		// This code below is in the camera plugin, FileHelper.java..
-		// TODO: Need to use that version if available.
 		// DocumentProvider
 		if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-			// ExternalStorageProvider
 			if (isExternalStorageDocument(uri)) {
 				final String docId = DocumentsContract.getDocumentId(uri);
 				final String[] split = docId.split(":");
@@ -235,20 +250,24 @@ public class FilePicker extends CordovaPlugin {
 				if ("primary".equalsIgnoreCase(type)) {
 					return Environment.getExternalStorageDirectory() + "/" + split[1];
 				}
+
 				// TODO handle non-primary volumes
 			}
 			// DownloadsProvider
 			else if (isDownloadsDocument(uri)) {
+
 				final String id = DocumentsContract.getDocumentId(uri);
-				final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+				final Uri contentUri = ContentUris.withAppendedId(
+					Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
 				return getDataColumn(context, contentUri, null, null);
 			}
 			// MediaProvider
-			else
-			if (isMediaDocument(uri)) {
+			else if (isMediaDocument(uri)) {
 				final String docId = DocumentsContract.getDocumentId(uri);
 				final String[] split = docId.split(":");
 				final String type = split[0];
+
 				Uri contentUri = null;
 				if ("image".equals(type)) {
 					contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
@@ -257,16 +276,22 @@ public class FilePicker extends CordovaPlugin {
 				} else if ("audio".equals(type)) {
 					contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
 				}
+
 				final String selection = "_id=?";
-				final String[] selectionArgs = new String[] {split[1]};
+				final String[] selectionArgs = new String[] {
+					split[1]
+				};
+
 				return getDataColumn(context, contentUri, selection, selectionArgs);
 			}
 		}
 		// MediaStore (and general)
 		else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
 			// Return the remote address
 			if (isGooglePhotosUri(uri))
 				return uri.getLastPathSegment();
+
 			return getDataColumn(context, uri, null, null);
 		}
 		// File
